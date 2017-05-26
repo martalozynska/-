81eb95d4876ecee140b9a7da50bde864e55e13b0
martalozynska <m.lozynska@ucu.edu.ua>
2017-05-26 22:11:11 +0300

    Повністю сформульована ідея та її опис

---
+++ modules/list_books.py
@@ -0,0 +1,21 @@
+'''
+Using Goodreads API to have a the database of books. Saving it 
+to the text file called 'book.txt' with the comand line.
+(python list_books.py > book.txt)
+'''
+from goodreads import client
+gc = client.GoodreadsClient('6YrisUQhu9CMAUHNVp2A', 'iQ0dZVXJk0W4Ci9FyYux7yKxUNfBXFtfK7fzYlk')
+for l in range(200, 300):
+    try:
+        book = gc.book(l)
+        print(book.title)
+    except Exception as e:
+        pass
+
+
+ 
+
+
+
+#6YrisUQhu9CMAUHNVp2A
+#secret: iQ0dZVXJk0W4Ci9FyYux7yKxUNfBXFtfK7fzYlk
--- modules/lookup_id.py
+++ modules/lookup_id.py
@@ -1,19 +1,45 @@
 import itunespy
-def lookup_id():
+import random
+from time import sleep
+
+def read_file():
     '''
-    Checks if the digit is the ID of book. If yes - writes to the file.
+    This function reads file called "book.txt" and saves the data
+    to the list, then formates it to set (if some books appear twice and more times)
+    and then back to list.
     '''
-    file = open('books.txt', 'w')
-    for i in range(100000000,1000000000):
+    lst = []
+    file = open("book.txt", 'r')
+    for line in file:
+        line = line.strip()
+        lst.append(line)
+    lst = set(lst)
+    return lst
+
+books = list(read_file())
+
+def find_book():
+    '''
+    Checks the list from the function read_file() whether the books are present 
+    in the database of iTunes Store. Then saves only the name of the book, the author
+    and price to the separate lists.
+    '''
+    book_attributes_needed = []
+    personal_lst = []
+    for book in books:
         try:
-            lookup = itunespy.lookup_book(id=i)
+            lookup = itunespy.search_book(book)
             if lookup[0].type == 'ebook':
-                lookup = str(lookup[0].track_id)
-                file.write(lookup + '\n')
-                print(lookup)
-        except LookupError:
-            print("Nothing")
-        except ConnectionError:
-            print("ZZzzzz...")
-            time.sleep(5)
-lookup_id()
+               book_attributes_needed.append(lookup[0].track_name)
+               book_attributes_needed.append(lookup[0].artist_name)
+               book_attributes_needed.append(lookup[0].price)
+               book_attributes_needed.extend([personal_lst])
+               personal_lst = []
+        except:
+            pass
+    
+    
+    return book_attributes_needed
+   
+find_book()
+
